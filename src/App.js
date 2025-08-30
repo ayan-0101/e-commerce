8acd7899f878345b7d1f5b7a53f4135e823c4d4e
ayan-0101 <ayanshakeel02@gmail.com>
2025-08-30 19:55:39 +0530

    Added product detail with rating

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Cart from "./customer/components/Cart/Cart";
 import Footer from "./customer/components/Footer/Footer";
 import Navigation from "./customer/components/Navigations/Navigation";
 import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
@@ -12,7 +13,8 @@ function App() {
       <div>
         {/* <HomePage/> */}
         {/* <ProductPage/> */}
-        <ProductDetails/>
+        {/* <ProductDetails/> */}
+        <Cart/>
       </div>
       <Footer/>
     </div>
